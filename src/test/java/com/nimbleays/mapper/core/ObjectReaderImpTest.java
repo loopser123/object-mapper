24384a19b09bb11feea60240833590a001118c3e
anas abahaddou <anas.abahaddou@nimbleways.com>
2020-09-24 22:15:52 +0100

    correct test

--- src/test/java/com/nimbleays/mapper/core/ObjectReaderImpTest.java
+++ src/test/java/com/nimbleays/mapper/core/ObjectReaderImpTest.java
@@ -1,5 +1,27 @@
 package com.nimbleays.mapper.core;
 
-public class ObjectReaderImpTest {
+
+
+import java.lang.reflect.Field;
+
+import org.junit.Test;
+
+import com.nimbleays.TestClassA;
+
+import junit.framework.TestCase;
+
+
+public class ObjectReaderImpTest  extends TestCase {
+	
+	
+	
+	@Test
+	public void testReadObjectASimpleObject() {
+		ObjectReaderImp objectReader = new ObjectReaderImp(); 
+		TestClassA a = new TestClassA("hamid","hamid","hamid"); 
+	    Field[] testObjectFields = objectReader.readObject(a); 
+		assertEquals(3,testObjectFields.length);
+			
+	}
 
 }
